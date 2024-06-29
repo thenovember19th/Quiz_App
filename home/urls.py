2e1045630c02872534c1df552f06e23c1f641f36
minrajbhatta.com <bhattaminraj88@gmail.com>
2024-06-29 14:43:18 +0545

    quizsamplecomplete

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.category_list, name='category_list'),
+    path('',views.home, name='home'),
+    path('categories/', views.category_list, name='category_list'),
     path('category/<int:category_id>/', views.quiz_list, name='quiz_list'),
     path('quiz/<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
     path('quiz/<int:quiz_id>/submit/', views.submit_quiz, name='submit_quiz'),
     path('quiz/<int:quiz_id>/results/', views.quiz_results, name='quiz_results'),
+    path('signup/', views.signUp, name='signup'),
+    path('login/', views.custom_login, name='customlogin'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Quiz, Question, Choice, UserQuiz
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_exempt
+
+def home(request):
+    return render(request, 'base.html')
 
 def category_list(request):
     categories = Category.objects.all()
@@ -10,7 +17,10 @@ def category_list(request):
 def quiz_list(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
     quizzes = category.quiz_set.all()
+    
     return render(request, 'quiz_list.html', {'category': category, 'quizzes': quizzes})
+pass
+
 
 @login_required
 def quiz_detail(request, quiz_id):
@@ -42,3 +52,42 @@ def quiz_results(request, quiz_id):
     # Handle case where no UserQuiz is found
         raise Http404("UserQuiz not found")
     return render(request, 'quiz_results.html', {'quiz': quiz, 'user_quiz': user_quiz})
+
+@csrf_exempt
+def signUp(request):
+    if request.method=="POST":
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        username=request.POST.get('username')
+        
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+    
+        user=User.objects.create_user(username,email,pass1)
+        user.first_name=fname
+        user.last_name=lname
+        user.save()
+        subject = 'Welcome TO Quiz Website'
+        message = f'Hi {user.username}, thank you for registering in newsportal.'
+        # email_from = settings.EMAIL_HOST_USER
+        # recipient_list = [user.email, ]
+        # send_mail( subject, message, email_from, recipient_list )
+        return redirect('login')
+   
+       
+    return render(request,'signup.html')
+
+@csrf_exempt
+def custom_login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'you are successfully logged in')
+            return redirect('category_list')
+        else:
+            messages.error(request,'invalid password or username')
+    return render(request,'login.html')
